1241ec05968bbfa3553d6120bc2aef1ad34354e8
Lori0107 <lori.benzemra@gmail.com>
2018-10-05 14:17:30 +0200

    last update - new API

--- controller.js
+++ controller.js
@@ -28,39 +28,57 @@ function addZero(i) {
 
 //localisation with latitude & longitude
   function myPosition(position) {
+    console.log('pass here first');
     var latitude = position.coords.latitude;
     var longitude = position.coords.longitude;
 
-//connexion with the weather API & data recovery
-    $http.get('https:lit-ridge-46374.herokuapp.com/forecast/a2e12f1f2fd8e8350976bececb546c6c/' + latitude + ',' + longitude + '?units=si')
-    .then(function (response, error){
-      if(response){
-        console.log(response)
-        $scope.apparentTemperature = response.data.currently.apparentTemperature;
-        $scope.summary = response.data.currently.summary;
-        $scope.temperatureHigh = response.data.daily.data[0].apparentTemperatureHigh;
-        $scope.temperatureLow = response.data.daily.data[0].apparentTemperatureLow;
-        $scope.humidity = response.data.currently.humidity;
-        $scope.precipitation = response.data.currently.precipIntensity;
-        $scope.windSpeed = response.data.currently.windSpeed;
-        $scope.icon = response.data.currently.icon;
-        $scope.eachHour = [];
-        for (i=0; i<24; i++){
-            $scope.eachHour[i] = response.data.hourly.data[i];
-          }
+    $http.get('http://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&units=metric&APPID=d075bfff2f9c02161dc50bc3cd404842')
+    .then(function (res, err) {
+      if(res) {
+        console.log(res)
+        $scope.city = res.data.name;
+        $scope.icon = res.data.weather[0].icon;
+        $scope.apparentTemperature = res.data.main.temp;
+        $scope.summary = res.data.weather[0].main;
+        $scope.temperatureHigh = res.data.main.temp_max;
+        $scope.temperatureLow = res.data.main.temp_min;
+        $scope.humidity = res.data.main.humidity;
+        $scope.windSpeed = res.data.wind.speed;
       } else {
-        console.log(error)
-      };
+        console.log(err)
+      }
     });
+//connexion with the weather API & data recovery
+    // $http.get('https://api.darksky.net/forecast/a2e12f1f2fd8e8350976bececb546c6c/' + latitude + ',' + longitude + '?units=si')
+    // .then(function (response, error){
+    //   if(response){
+    //     console.log(response)
+    //     $scope.apparentTemperature = response.data.currently.apparentTemperature;
+    //     $scope.summary = response.data.currently.summary;
+    //     $scope.temperatureHigh = response.data.daily.data[0].apparentTemperatureHigh;
+    //     $scope.temperatureLow = response.data.daily.data[0].apparentTemperatureLow;
+    //     $scope.humidity = response.data.currently.humidity;
+    //     $scope.precipitation = response.data.currently.precipIntensity;
+    //     $scope.windSpeed = response.data.currently.windSpeed;
+    //     $scope.icon = response.data.currently.icon;
+    //     $scope.eachHour = [];
+    //     for (i=0; i<24; i++){
+    //         $scope.eachHour[i] = response.data.hourly.data[i];
+    //       }
+    //   } else {
+    //     console.log(error)
+    //   };
+    // });
+
   };
 
-  $http.get("https://ipinfo.io?token=9c6563290a7f05").then(function (response, err) {
-    if(response){
-      console.log(response);
-      $scope.city = response.data.city
-    } else {
-      console.log(err)
-    }
-  });
+  // $http.get("https://ipinfo.io?token=9c6563290a7f05").then(function (response, err) {
+  //   if(response){
+  //     console.log(response);
+  //     $scope.city = response.data.city
+  //   } else {
+  //     console.log(err)
+  //   }
+  // });
 
 });
