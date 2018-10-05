2caf767cd7090b7a8ba5c686c33d853663300630
Lori0107 <lori.benzemra@gmail.com>
2018-10-05 14:24:05 +0200

    last update - cleaning code

--- controller.js
+++ controller.js
@@ -48,37 +48,5 @@ function addZero(i) {
         console.log(err)
       }
     });
-//connexion with the weather API & data recovery
-    // $http.get('https://api.darksky.net/forecast/a2e12f1f2fd8e8350976bececb546c6c/' + latitude + ',' + longitude + '?units=si')
-    // .then(function (response, error){
-    //   if(response){
-    //     console.log(response)
-    //     $scope.apparentTemperature = response.data.currently.apparentTemperature;
-    //     $scope.summary = response.data.currently.summary;
-    //     $scope.temperatureHigh = response.data.daily.data[0].apparentTemperatureHigh;
-    //     $scope.temperatureLow = response.data.daily.data[0].apparentTemperatureLow;
-    //     $scope.humidity = response.data.currently.humidity;
-    //     $scope.precipitation = response.data.currently.precipIntensity;
-    //     $scope.windSpeed = response.data.currently.windSpeed;
-    //     $scope.icon = response.data.currently.icon;
-    //     $scope.eachHour = [];
-    //     for (i=0; i<24; i++){
-    //         $scope.eachHour[i] = response.data.hourly.data[i];
-    //       }
-    //   } else {
-    //     console.log(error)
-    //   };
-    // });
-
   };
-
-  // $http.get("https://ipinfo.io?token=9c6563290a7f05").then(function (response, err) {
-  //   if(response){
-  //     console.log(response);
-  //     $scope.city = response.data.city
-  //   } else {
-  //     console.log(err)
-  //   }
-  // });
-
 });
